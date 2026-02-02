4603edf8eaeebf5db8513f5d90960b7cfd370040
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-02-02 14:36:34 +0200

    Patient Insurance + Insurance Coverage (#38)
    * Patient Insurance + Insurance Coverage
    
    * LV
    
    * lv

---
+++ src/main/java/com/dazzle/asklepios/domain/PatientInsurance.java
@@ -0,0 +1,69 @@
+package com.dazzle.asklepios.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(  name = "patient_insurances")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientInsurance extends AbstractAuditingEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @NotNull
+    @Column(name = "payor_id", nullable = false)
+    private Long payorId;
+
+    @NotNull
+    @Column(name = "plan_id")
+    private Long planId;
+
+    @Column(name = "policy_holder_id")
+    private Long policyHolderId;
+
+    @NotNull
+    @Column(name = "policy_number", nullable = false)
+    private Long policyNumber;
+
+    @Column(name = "group_number")
+    private Long groupNumber;
+
+    @Future @NotNull
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(name = "remaining_benefits", precision = 19, scale = 2)
+    private BigDecimal remainingBenefits;
+
+    @Column(name = "remaining_deductibles", precision = 19, scale = 2)
+    private BigDecimal remainingDeductibles;
+
+    @Column(name = "is_primary", nullable = false)
+    private Boolean isPrimary = false;
+}
---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/BillingItemTypes.java
@@ -0,0 +1,10 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum BillingItemTypes {
+    MEDICATION,
+    LABORATORY,
+    RADIOLOGY,
+    PATHOLOGY,
+    SERVICE,
+    PROCEDURE
+}
