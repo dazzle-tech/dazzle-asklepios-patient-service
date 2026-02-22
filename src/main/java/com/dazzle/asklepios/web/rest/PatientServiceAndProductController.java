635926be82d44494124bbbad23d90af284e7bdef
Rawan-Yahya200 <rawanyahya300@gmail.com>
2026-02-22 11:41:07 +0200

    Features/ASK-928 patient services and products fixes

--- src/main/java/com/dazzle/asklepios/web/rest/PatientServiceAndProductController.java
+++ src/main/java/com/dazzle/asklepios/web/rest/PatientServiceAndProductController.java
@@ -14,7 +14,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -37,12 +44,12 @@ public class PatientServiceAndProductController {
 
     @PostMapping("/patient-services-products")
     public ResponseEntity<PatientServiceAndProduct> create(
-            @Valid @RequestBody PatientServiceProductCreateDTO dto
+            @Valid @RequestBody PatientServiceProductCreateDTO patientServiceProductCreateDTO
     ) {
 
-        LOG.debug("REST create Patient Service/Product payload={}", dto);
+        LOG.debug("REST create Patient Service/Product payload={}", patientServiceProductCreateDTO);
 
-        PatientServiceAndProduct created = patientServiceAndProductService.create(dto);
+        PatientServiceAndProduct created = patientServiceAndProductService.create(patientServiceProductCreateDTO);
 
         LOG.debug("REST create Patient Service/Product response={}", created);
 
@@ -75,13 +82,13 @@ public class PatientServiceAndProductController {
     @PutMapping("/patient-services-products/{id}")
     public ResponseEntity<PatientServiceAndProduct> update(
             @PathVariable Long id,
-            @Valid @RequestBody PatientServiceProductUpdateDTO dto
+            @Valid @RequestBody PatientServiceProductUpdateDTO patientServiceProductUpdateDTO
     ) {
 
-        LOG.debug("REST update Patient Service/Product id={} payload={}", id, dto);
+        LOG.debug("REST update Patient Service/Product id={} payload={}", id, patientServiceProductUpdateDTO);
 
         PatientServiceAndProduct updated =
-                patientServiceAndProductService.update(dto);
+                patientServiceAndProductService.update(patientServiceProductUpdateDTO);
 
         return ResponseEntity.ok(updated);
     }
