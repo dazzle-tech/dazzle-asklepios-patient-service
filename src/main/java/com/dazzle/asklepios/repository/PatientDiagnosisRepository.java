9b081cae1c67a8cfe8735e81933e0ddf4949464a
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-02-25 13:43:18 +0200

    Clinical Visit (#54)
    * Clinical Visit
    
    * LV
    
    * LV
    
    ---------
    
    Co-authored-by: Qaisnfi7 <qais.farouk@gmail.com>

---
+++ src/main/java/com/dazzle/asklepios/repository/PatientDiagnosisRepository.java
@@ -0,0 +1,18 @@
+package com.dazzle.asklepios.repository;
+
+import com.dazzle.asklepios.domain.PatientDiagnosis;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PatientDiagnosisRepository extends JpaRepository<PatientDiagnosis, Long> {
+
+    Optional<PatientDiagnosis> findTopByEncounterIdOrderByCreatedDateDesc(Long encounterId);
+
+    Page<PatientDiagnosis> findByPatientIdOrderByCreatedDateDesc(
+            Long patientId,
+            Pageable pageable
+    );
+}
---
+++ src/main/java/com/dazzle/asklepios/service/dto/patientDiagnosis/PatientDiagnosisCreateDTO.java
@@ -0,0 +1,31 @@
+package com.dazzle.asklepios.service.dto.patientDiagnosis;
+
+import com.dazzle.asklepios.domain.enumeration.DiagnosisType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record PatientDiagnosisCreateDTO(
+
+        @NotNull
+        Long patientId,
+
+        @NotNull
+        Long encounterId,
+
+        @NotNull
+        Long diagnosisId,
+
+        @NotNull
+        DiagnosisType type,
+
+        @NotNull
+        Boolean suspected,
+
+        @NotNull
+        Boolean major
+
+) implements Serializable {
+}
