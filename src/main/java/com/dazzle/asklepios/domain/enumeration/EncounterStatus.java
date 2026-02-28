aa48232edfca6c456a8707d9b631b239eba8737e
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-02-28 15:16:10 +0200

    Rename CANCELED to CANCELLED for consistency

--- src/main/java/com/dazzle/asklepios/domain/enumeration/EncounterStatus.java
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/EncounterStatus.java
@@ -3,7 +3,7 @@ package com.dazzle.asklepios.domain.enumeration;
 public enum EncounterStatus {
     NEW,
     ONGOING,
-    CANCELED,
+    CANCELLED,
     CLOSED,
     DISCHARGED,
     IN_OPERATION,
