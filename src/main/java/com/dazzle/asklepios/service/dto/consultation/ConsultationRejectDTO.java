6110685dcf5a9102f5d5e138c6119ac262abd486
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-03-10 22:08:07 +0200

    fix consultation (#123)

--- src/main/java/com/dazzle/asklepios/service/dto/consultation/ConsultationRejectDTO.java
+++ src/main/java/com/dazzle/asklepios/service/dto/consultation/ConsultationRejectDTO.java
@@ -1,9 +1,10 @@
 package com.dazzle.asklepios.service.dto.consultation;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 
 import java.io.Serializable;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ConsultationRejectDTO(
 
         @NotBlank(message = "Reject reason is required")
