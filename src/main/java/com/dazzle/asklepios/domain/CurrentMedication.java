a8f0c69e63d572d7ecbcc9984282fca94cfd31a5
Qaisnfi7 <qais.farouk@gmail.com>
2026-04-07 16:01:40 +0300

    v1 (#147)

---
+++ src/main/java/com/dazzle/asklepios/domain/CurrentMedication.java
@@ -0,0 +1,44 @@
+package com.dazzle.asklepios.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "current_medication")
+@EqualsAndHashCode(callSuper = false)
+public class CurrentMedication extends AbstractAuditingEntity<Long>
+        implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @Column(name = "active_ingredient_id", nullable = false)
+    private Long activeIngredientId;
+
+    @Column(name = "instructions", columnDefinition = "text")
+    private String instructions;
+
+    @NotNull
+    @Column(name = "start_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+}
---
+++ src/main/java/com/dazzle/asklepios/repository/CurrentMedicationRepository.java
@@ -0,0 +1,11 @@
+package com.dazzle.asklepios.repository;
+
+import com.dazzle.asklepios.domain.CurrentMedication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrentMedicationRepository extends JpaRepository<CurrentMedication, Long> {
+
+    Page<CurrentMedication> findAllByPatientId(Long patientId, Pageable pageable);
+}
