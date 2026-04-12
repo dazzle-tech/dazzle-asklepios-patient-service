a278b8822a7a60056c4d107ea23361143c0d04e3
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-04-12 16:26:28 +0300

    Update AppointmentFromTemplateService.java

--- src/main/java/com/dazzle/asklepios/service/AppointmentFromTemplateService.java
+++ src/main/java/com/dazzle/asklepios/service/AppointmentFromTemplateService.java
@@ -34,6 +34,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.ArrayList;
