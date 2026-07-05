20a3764e864b19c3b41a961824b3d81ad447af2a
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-07-05 11:54:12 +0300

    Service & Product // search

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/CoverageStatus.java
@@ -0,0 +1,10 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum CoverageStatus {
+    NOT_CHECKED,
+    COVERED,
+    NOT_COVERED,
+    PARTIALLY_COVERED,
+    REJECTED,
+    ERROR
+}
---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/PriceSource.java
@@ -0,0 +1,9 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum PriceSource {
+    MANUAL,
+    PRICE_LIST,
+    INSURANCE_CONTRACT,
+    WASEEL_SBS,
+    DEFAULT
+}
