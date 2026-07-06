a30ba9ab9c0208e4b186dd959853ba787ccbb312
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-07-06 10:43:55 +0300

    cancel prescription

--- src/main/java/com/dazzle/asklepios/web/rest/PatientPrescriptionController.java
+++ src/main/java/com/dazzle/asklepios/web/rest/PatientPrescriptionController.java
@@ -99,10 +99,10 @@ public class PatientPrescriptionController {
 
     @PostMapping("/patient-prescriptions/{id}/cancel")
     public ResponseEntity<PatientPrescription> cancel(
-            @PathVariable Long id,
-            @RequestParam String lastModifiedBy
+            @PathVariable Long id
+
     ) {
         LOG.debug("cancel prescription for id ={}",id);
-        return ResponseEntity.ok(patientPrescriptionService.cancel(id, lastModifiedBy));
+        return ResponseEntity.ok(patientPrescriptionService.cancel(id));
     }
 }
