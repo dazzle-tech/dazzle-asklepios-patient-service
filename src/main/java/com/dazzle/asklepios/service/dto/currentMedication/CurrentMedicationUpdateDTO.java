458106a9d38b711afb42cc4bc0f0afbafde3541f
Rawan-Yahya200 <rawanyahya300@gmail.com>
2026-07-19 12:15:31 +0300

    replace instructions with dosage fields

--- src/main/java/com/dazzle/asklepios/service/dto/currentMedication/CurrentMedicationUpdateDTO.java
+++ src/main/java/com/dazzle/asklepios/service/dto/currentMedication/CurrentMedicationUpdateDTO.java
@@ -1,10 +1,13 @@
 package com.dazzle.asklepios.service.dto.currentMedication;
 
+import com.dazzle.asklepios.domain.enumeration.MedFrequency;
+import com.dazzle.asklepios.domain.enumeration.UOM;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -19,7 +22,11 @@ public record CurrentMedicationUpdateDTO(
         @NotNull
         Long activeIngredientId,
 
-        String instructions,
+        BigDecimal dosage,
+
+        UOM unit,
+
+        MedFrequency frequency,
 
         @NotNull
         @PastOrPresent
