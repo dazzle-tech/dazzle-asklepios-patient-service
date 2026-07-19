812a47ba593e310ae7c7c83c13c9557e801800ef
Rawan-Yahya200 <rawanyahya300@gmail.com>
2026-07-19 11:04:26 +0300

    Replace free-text instructions with structured dosage fields in Current Medication

--- src/main/java/com/dazzle/asklepios/service/dto/currentMedication/CurrentMedicationCreateDTO.java
+++ src/main/java/com/dazzle/asklepios/service/dto/currentMedication/CurrentMedicationCreateDTO.java
@@ -1,10 +1,13 @@
 package com.dazzle.asklepios.service.dto.currentMedication;
 
+import com.dazzle.asklepios.domain.enumeration.MedFrequency;
+import com.dazzle.asklepios.domain.enumeration.UOM;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -16,7 +19,11 @@ public record CurrentMedicationCreateDTO(
         @NotNull
         Long activeIngredientId,
 
-        String instructions,
+        BigDecimal dosage,
+
+        UOM unit,
+
+        MedFrequency frequency,
 
         @NotNull
         @PastOrPresent
