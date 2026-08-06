32bc21038c57083de9bb0a676843f00994a6a927
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-08-06 17:32:34 +0300

    Update PacsPatientDTO.java

--- src/main/java/com/dazzle/asklepios/service/dto/radiology/PacsPatientDTO.java
+++ src/main/java/com/dazzle/asklepios/service/dto/radiology/PacsPatientDTO.java
@@ -1,9 +1,19 @@
 package com.dazzle.asklepios.service.dto.radiology;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record PacsPatientDTO(
+
         String id,
+
+        @JsonProperty("first_name")
         String firstName,
+
+        @JsonProperty("second_name")
         String secondName,
+
+        @JsonProperty("last_name")
         String lastName
+
 ) {
 }
