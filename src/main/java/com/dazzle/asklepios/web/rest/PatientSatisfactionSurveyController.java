404309fc7e029e8f700d774df34b96bee9cbf2db
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-08-10 17:02:38 +0300

    list

--- src/main/java/com/dazzle/asklepios/web/rest/PatientSatisfactionSurveyController.java
+++ src/main/java/com/dazzle/asklepios/web/rest/PatientSatisfactionSurveyController.java
@@ -9,7 +9,10 @@ import com.dazzle.asklepios.web.rest.vm.PatientSatisfactionSurveyResponseVM;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +36,14 @@ public class PatientSatisfactionSurveyController {
 
         return ResponseEntity.ok(
                 mapToVM(response)
+        );
+    }
+    @GetMapping("/patient-satisfaction-survey/responses")
+    public ResponseEntity<Page<PatientSatisfactionSurveyResponseVM>> getSubmittedSurveys(
+            Pageable pageable) {
+
+        return ResponseEntity.ok(
+                patientSatisfactionSurveyService.getSubmittedSurveys(pageable)
         );
     }
     private PatientSatisfactionSurveyResponseVM mapToVM(
