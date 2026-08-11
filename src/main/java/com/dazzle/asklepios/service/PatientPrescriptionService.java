99259aaa3bb2645c5bf827ffd3c9c9cf1856b859
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-08-11 14:56:54 +0300

    edit field

--- src/main/java/com/dazzle/asklepios/service/PatientPrescriptionService.java
+++ src/main/java/com/dazzle/asklepios/service/PatientPrescriptionService.java
@@ -175,7 +175,7 @@ public class PatientPrescriptionService {
             Long encounterId,
             PrescriptionStatus status,
             PrescriptionUrgencyLevel urgencyLevel,
-            Long prescriptionNum,
+            String prescriptionNum,
             boolean includeCanceled,
             Pageable pageable
     ) {
