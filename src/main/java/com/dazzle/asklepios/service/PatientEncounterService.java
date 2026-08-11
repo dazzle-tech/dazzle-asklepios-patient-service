3d7c188b3d0d9be4850967e8837898833b2a5c5f
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-08-11 21:56:53 +0300

    Update PatientEncounterService.java (#361)

--- src/main/java/com/dazzle/asklepios/service/PatientEncounterService.java
+++ src/main/java/com/dazzle/asklepios/service/PatientEncounterService.java
@@ -1299,6 +1299,7 @@ public class PatientEncounterService {
             PatientEncounter saved = patientEncounterRepository.saveAndFlush(encounter);
 
             encounterAssignToBedService.dischargeActiveAssignmentByEncounterId(saved.getId());
+            notifyEncounterEvent(saved, NotificationCode.ENCOUNTER_CLOSED, null);
 
             LOG.info("[DISCHARGE] success id={} status={} dischargeType={} dischargeAt={}",
                     saved.getId(),
