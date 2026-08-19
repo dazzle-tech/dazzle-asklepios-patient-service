1ab71cd4dcb1dd1b193991ec9c4d2f2317be9702
Faroukdhelea <fdhelea@gmail.com>
2026-08-19 09:46:40 +0300

    ask-2639-fx

--- src/main/java/com/dazzle/asklepios/service/PatientEncounterService.java
+++ src/main/java/com/dazzle/asklepios/service/PatientEncounterService.java
@@ -855,6 +855,7 @@ public class PatientEncounterService {
                 );
             }
 
+
             predicates.add(
                     cb.between(
                             root.get("encounterDate"),
