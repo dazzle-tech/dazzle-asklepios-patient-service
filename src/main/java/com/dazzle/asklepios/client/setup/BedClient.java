19bbc6ce6cdb2c1cb155a310df89674a753b6de1
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-09-20 16:16:27 +0300

    Ask bed issue (#443)
    * invoice coverage
    
    * fix

--- src/main/java/com/dazzle/asklepios/client/setup/BedClient.java
+++ src/main/java/com/dazzle/asklepios/client/setup/BedClient.java
@@ -1,15 +1,25 @@
 package com.dazzle.asklepios.client.setup;
 
+import com.dazzle.asklepios.client.setup.dto.BedDTO;
 import com.dazzle.asklepios.config.SetupServiceFeignConfig;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "setupServiceClient",url = "${service.asklepios-setup-service-url}" , configuration = SetupServiceFeignConfig.class)
+@FeignClient(
+        name = "setupServiceClient",
+        contextId = "bedClient",
+        url = "${service.asklepios-setup-service-url}",
+        configuration = SetupServiceFeignConfig.class
+)
 public interface BedClient {
 
     @GetMapping("/api/setup/bed/{id}")
-    ResponseEntity<Void> existsBed(@PathVariable("id") @NotNull Long bedId );
+    ResponseEntity<Void> existsBed(@PathVariable("id") @NotNull Long bedId);
+
+    @PostMapping("/api/setup/bed/{id}/mark-in-cleaning")
+    BedDTO markAsInCleaning(@PathVariable("id") @NotNull Long bedId);
 }
